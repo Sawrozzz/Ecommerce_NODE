9da63f5097a4e6e41cc5989ca62d035c7aff926e
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-06-15 12:28:03 +0545

    refactoring routes

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const ownerModel = require("../models/owner-model");
 
 const router = express.Router();
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res.status(503).send("You don't have permission to create owner");
+    }
+    let { fullname, email, password } = req.body;
+    try {
+      let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+      });
+      res.status(201).send(createdOwner);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred while creating the owner.");
+    }
+  });
+}
+
 router.get("/", function (req, res) {
   res.send("Owners route");
 });
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const userModel = require('../models/user-model')
 
 const router = express.Router();
 router.get("/",function(req,res){
     res.send("Working on users route");
 });
+
 
 module.exports = router;
