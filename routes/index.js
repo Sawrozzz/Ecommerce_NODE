ee78ac1d5f60a0294e6cbefce117d9a8174c6a9a
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-06-26 06:50:41 +0545

    adding new routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express"); 
 const router = express.Router();
+const isLoggedIn = require("../middlewares/isLoggedIn");
+
 router.get("/", function (req, res) {
-  res.render("index");
+  let error = req.flash("error");
+  res.render("index",{error});
 });
 
-router.get("/owner-login",function(req,res){
-  res.render("owner-login");
+router.get("/shop", isLoggedIn, function (req, res) {
+  res.render("shop");
 });
 
 
