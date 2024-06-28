4a1a2ef927f33a0ddb18cadf0899f755b9a0740d
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-06-28 18:35:13 +0545

    complete login, reigisterand logout controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,13 @@
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
-const { generateToken } = require("../utils/generateToken")
+const { generateToken } = require("../utils/generateToken");
 
 module.exports.registerUser = async function (req, res) {
   try {
     let { email, fullname, password } = req.body;
 
-  let user = await userModel.findOne({ email:email});
-  if(user)
-    return res.status(401).send("User already exists")
-
+    let user = await userModel.findOne({ email: email });
+    if (user) return res.status(401).send("User already exists");
 
     bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, async function (err, hash) {
@@ -32,28 +30,36 @@ module.exports.registerUser = async function (req, res) {
 };
 
 module.exports.loginUser = async function (req, res) {
-    let { email, password } = req.body;
+  let { email, password } = req.body;
 
-   let user = await userModel.findOne({email:email});
-   if(!user)
-    return res.send("Email or password is incorrect");  
- 
-    bcrypt.compare(password, user.password, function(err, result){
-        if(result){
-            let token = generateToken(user);
-            res.cookie("token", token); 
-            res.send("User logged in successfully");
-            // res.redirect("/shop");
-        }
-        else{
-            res.status(401).send("Email or password is incorrect");
-        }
+  let user = await userModel.findOne({ email: email });
+  if (!user) {
+    req.flash("error", "User does not exist");
+    return res.redirect("/");
+  }
 
-
- })
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      let token = generateToken(user);
+      res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
+      res.redirect("/shop");
+    } else {
+      res.status(401).send("Email or password is incorrect");
+    }
+  });
 };
 
 module.exports.logoutUser = function (req, res) {
-  res.clearCookie("token", token);
-  res.send("User logged out successfully");
-}
+  res.clearCookie("token", {
+    path: "/",
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
+  res.redirect("/");
+};
