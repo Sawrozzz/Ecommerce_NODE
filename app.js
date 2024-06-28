fc1517946c2e9ea8a0aafcb55b7a405fca3e8fb9
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-06-28 18:37:50 +0545

    refactor

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const express = require('express'); 
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const flash = require("connect-flash");
+const expressSession = require("express-session");
 
-const ownersRouter = require('./routes/ownersRouter');
-const productsRouter = require('./routes/productsRouter');
-const usersRouter = require('./routes/usersRouter');
+const indexRouter = require("./routes/index");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
 
-const db = require('./config/mongoose-connection');
+const db = require("./config/mongoose-connection");
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
+app.use("/", indexRouter);
+app.use("/owners", ownersRouter);
+app.use("/products", productsRouter);
+app.use("/users", usersRouter);
 
-app.use('/owners',ownersRouter);
-app.use('/products',productsRouter);
-app.use('/users',usersRouter);
-  
 app.listen(3000);
