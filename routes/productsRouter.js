7925509a1806bd4ccfc7cf632f07e569420d70c4
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-06-29 12:26:11 +0545

    used multer in product/create route

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,11 +1,30 @@
 const express = require("express");
-
 const router = express.Router();
-router.get("/", function (req, res) {
-  res.send("Products route");
-});
-router.get("/create", function (req, res) {
-  res.render("createproducts");
+
+const upload = require("../config/multer-config");
+
+const productModel = require("../models/product-model");
+
+router.post("/create", upload.single("image"), async function (req, res) {
+  try {
+    let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+    if (!req.file || !req.file.buffer) {
+      return res.status(400).send("No file uploaded");
+    }
+    let product = await productModel.create({
+      image: req.file.buffer,
+      name,
+      discount,
+      price,
+      bgcolor,
+      panelcolor,
+      textcolor,
+    });
+    req.flash("success", "Product created successfully");
+    res.redirect("/owners/admin");
+  } catch {
+    res.send(err.message);
+  }
 });
 
 module.exports = router;
